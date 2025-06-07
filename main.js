56721c63fd80d37c01efac1958e19249e68530ef
Co01ley <abobbietoonalt6@gmail.com>
2025-06-07 10:17:00 +0000

    done

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ const chatBoxBody = chatBox.querySelector('.chat-box-body');
 
 // Create sendMessade Function
 button.addEventListener('click', sendMessage);
+inputField.addEventListener('keypress'), function (event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+
+    }
+}
+
+function sendMessage() {
+    const message = inputField.value;
+    inputField.value = '';
+    chatBoxBody.innerHTML += '<div class="message">${message}</div>';
+    scrollToBottom();
+
+    fetch('http://localhost:3000/message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message })
+    }).then(response => response.json())
+    .then(data => {
+        chatBoxBody.innerHTML += '<div class="response">${data.message}</div>';
+        scrollToBottom();
+    
+    });
+}
+
+function scrollToBottom() {
+    chatBoxBody.scrollTop = chatBoxBody.scrollHeight;
+}
